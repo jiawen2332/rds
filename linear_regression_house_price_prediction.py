320c3cb8f710ad6c80eaa9dca235e3263c5e57a7
jiawen2332 <sumjw-wp24@student.tarc.edu.my>
2026-07-16 13:42:12 +0800

    Update linear_regression_house_price_prediction.py

--- linear_regression_house_price_prediction.py
+++ linear_regression_house_price_prediction.py
@@ -15,8 +15,8 @@ model = load ('linear_regression_model.joblib')
 
 user_input = st.number_input('Enter house size: ', min_value=100,max_value=10000,step=50)
 
-input_array = np.array([user_input]).reshape(-1.1)
+input_array = np.array([user_input]).reshape(-1,1)
 
 if st.button('Predict Price'):
   predicted_price = model.predict(input_array)
-  st.write(f'The predicted house price is: {predicted_price[0]:.2f}')
+  st.write(f'The predicted house price is: ${predicted_price[0]:.2f}')
